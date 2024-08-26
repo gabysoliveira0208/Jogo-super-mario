585917804bbd8dfcab532defbde8a8e69a681d1c
gabysoliveira0208 <163007831+gabysoliveira0208@users.noreply.github.com>
2024-08-26 10:32:16 -0300

    .

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
 const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
 
 const jump = () => {
      mario.classList.add('jump');
-}                       
+
+     setTimeout(() => {
+          mario.classList.remove("jump");
+     }
+ ,500);
+}          
+
+const loop = setInterval(() => {});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.addEventlistener('keydown', jump);
-  
-  const pipe 
-  
