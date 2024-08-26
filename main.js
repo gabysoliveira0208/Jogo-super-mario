0464e23fc06d71b8e701d19b646a02f3f5d31079
gabysoliveira0208 <163007831+gabysoliveira0208@users.noreply.github.com>
2024-08-26 07:53:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+const mario = document.querySelector(".mario");
 
+const jump = () => {
+     mario.classList.add('jump');
+}                       
+document.addEventlistener('keydown', jump);
+  
+  const pipe 
+  
